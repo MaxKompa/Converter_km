c208afe03b7ce1a08662afbc3f867da9593a60ac
MaxKompa <max.kompa645@gmail.com>
2024-06-10 19:19:36 +0200

    Add km to miles convert & other variable values

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,13 +7,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter App.");
 
-        double miles = 5.0;
+        double miles = 8.0;
+        double kms = 15.0;
         double km = convertMilesToKilometers(miles);
+        double mile = convertKilometersToMiles(kms);
 
-        System.out.println("Result is " + km + " kilometers.");
+        System.out.println("Result is " + km + " kilometers and " + mile + " miles.");
     }
 
     public static double convertMilesToKilometers(double miles) {
         return miles * CONV_K;
+    }
+
+    public static double convertKilometersToMiles(double kms) {
+        return kms / CONV_K;
     }
 }
